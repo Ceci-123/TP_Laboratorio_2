7bff907b0dd60de71e45061cfa445f5f5708bdbb
Ceci <78119165+Ceci-123@users.noreply.github.com>
2022-04-20 12:29:25 -0300

    segundo parcial terminado

---
+++ TP 2/Entidades/Ciclomotor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+            
+        }
+
+        /// <summary>
+        /// Muestra los datos de un objeto de tipo ciclomotor
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
